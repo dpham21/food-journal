47e2df43f49d2c58bce3179f81c6452d2c4597a8
Daniel Pham <15683135+dpham21@users.noreply.github.com>
2023-04-14 01:11:58 -0400

    add blk-design-system-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { CarouselModule } from "ngx-bootstrap/carousel";
+import { JwBootstrapSwitchNg2Module } from "jw-bootstrap-switch-ng2";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    RouterModule,
+    AppRoutingModule,
+    JwBootstrapSwitchNg2Module,
+    CarouselModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
